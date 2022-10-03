99e629618f0ca1707f853fffe95d21fe69d7b326
Dennis Ramos <dsrg1995@gmail.com>
2022-10-02 23:32:42 -0500

    Ultimos ajustes

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -2,6 +2,9 @@ import 'dart:io';
 
 class Environment {
 
-  static String apiUrl    = Platform.isAndroid  ? 'http://31.220.31.215:3000/api' : 'http://31.220.31.215:3000/api';
-  static String socketUrl = Platform.isAndroid  ? 'http://31.220.31.215:3000' : 'http://31.220.31.215:3000';
+  static String apiUrl    = Platform.isAndroid  ? 'http://127.0.0.1:3000/api' : 'http://localhost:3000/api';
+  static String socketUrl = Platform.isAndroid  ? 'http://127.0.0.1:3000' : 'http://localhost:3000';
+  
+  // static String apiUrl    = Platform.isAndroid  ? 'http://31.220.31.215:3000/api' : 'http://31.220.31.215:3000/api';
+  // static String socketUrl = Platform.isAndroid  ? 'http://31.220.31.215:3000' : 'http://31.220.31.215:3000';
 }
